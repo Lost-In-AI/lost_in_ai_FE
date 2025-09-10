5539e01e29a4732c31edaa784998dc889ec149b1
Amy <emysgalippa@gmail.com>
2025-09-10 15:17:45 +0200

    implemented iu & message components (closes #22)

---
+++ src/components/LoadingIcon.tsx
@@ -0,0 +1,63 @@
+interface LoadingIconProps {
+  color: string;
+}
+export default function LoadingIcon({ color }: LoadingIconProps) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200">
+      <circle fill={color} stroke-width="20" r="20" cx="40" cy="65">
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;110;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.4"
+        ></animate>
+        <animate
+          attributeName="opacity"
+          dur="2"
+          values="0.3;1;0.3;"
+          repeatCount="indefinite"
+          begin="-.4"
+        ></animate>
+      </circle>
+      <circle fill={color} stroke={color} stroke-width="20" r="20" cx="100" cy="65">
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;110;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="-.2"
+        ></animate>
+        <animate
+          attributeName="opacity"
+          dur="2"
+          values="0.3;1;0.3;"
+          repeatCount="indefinite"
+          begin="-.2"
+        ></animate>
+      </circle>
+      <circle fill={color} stroke={color} stroke-width="20" r="20" cx="160" cy="65">
+        <animate
+          attributeName="cy"
+          calcMode="spline"
+          dur="2"
+          values="65;110;65;"
+          keySplines=".5 0 .5 1;.5 0 .5 1"
+          repeatCount="indefinite"
+          begin="0"
+        ></animate>
+        <animate
+          attributeName="opacity"
+          dur="2"
+          values="0.3;1;0.3;"
+          repeatCount="indefinite"
+          begin="0"
+        ></animate>
+      </circle>
+    </svg>
+  );
+}
---
+++ src/components/chat/ChatWrapper.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef, useState, type ReactNode } from "react";
+import Header from "./Header";
+import { useSessionStore } from "../../store/useSessionStore";
+import { useChatStatusStore } from "../../store/useChatStatusStore";
+import Input from "./Input";
+import MessageLoading from "./MessageLoading";
+
+interface ChatWrapperProps {
+  children?: ReactNode;
+}
+
+export default function ChatWrapper({ children }: ChatWrapperProps) {
+  const [isVisible, setIsVisible] = useState(false);
+  const chatContainerRef = useRef<HTMLDivElement>(null);
+  const { sessionData } = useSessionStore();
+  const { loading } = useChatStatusStore();
+
+  useEffect(() => {
+    if (chatContainerRef.current) {
+      const container = chatContainerRef.current;
+      container.scrollTo({
+        top: container.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+  }, [children, sessionData]);
+
+  return (
+    <section className="fixed inset-0 flex pointer-events-none flex-col">
+      {isVisible && (
+        <div 
+          tabIndex={0}
+          className="flex flex-col w-[95%] h-[80vh] sm:max-w-[1199px] sm:absolute sm:right-12 sm:bottom-4 mx-auto mt-4 mb-24 backdrop-blur-sm bg-white p-0.5 rounded-lg pointer-events-auto shadow-lg focus:outline-none focus:ring-2 focus:ring-primary-700/50"
+        >
+          <div ref={chatContainerRef} className="flex-1 overflow-y-auto p-4">
+            <Header onClose={() => setIsVisible(!isVisible)} />
+            {children}
+            {loading === "pending" && <MessageLoading />}
+          </div>
+          <Input />
+        </div>
+      )}
+      {/* TODO: mettere il botton vero */}
+      <button
+        className="bg-primary-700 absolute bottom-4 text-white right-4 md:bottom-8 md:right-12 px-4 w-16 h-16 rounded-lg pointer-events-auto "
+        onClick={() => setIsVisible(!isVisible)}
+      >
+        {isVisible ? "Close" : "Open"}
+      </button>
+    </section>
+  );
+}
