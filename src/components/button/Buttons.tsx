cabffe66b404c785d2e4332673fab7f2de522562
Virginia <virginia.tgnt@gmail.com>
2025-09-13 16:23:19 +0200

    token

--- src/components/button/Buttons.tsx
+++ src/components/button/Buttons.tsx
@@ -18,30 +18,30 @@ variant="personality" bottone per lo switch della personality
 variant="close" bottone per chiudere la chat con il bot 
 */
 
-
 const variants = {
-    primary:"bg-transparent",
-    secondary: "bg-transparent",
-    personality:"bg-transparent",
-    close:"bg-transparent",
-}
+  primary: "bg-transparent",
+  secondary: "bg-transparent",
+  personality: "bg-transparent",
+  close: "bg-transparent",
+};
 
 const variantIcons = {
-  primary: <img src={botImage} alt="bot" className="rounded-full "/>,
-  secondary:<img src={send} alt="secondary" />,
+  primary: <img src={botImage} alt="bot" className="rounded-full " />,
+  secondary: <img src={send} alt="secondary" />,
   personality: <img src={personality} alt="personality" />,
-  close: <img src={close} alt="close"/>,
-}
+  close: <img src={close} alt="close" />,
+};
 
-export default function Buttons ({text, onClick, variant, disabled=false, className } : ButtonsProps) {
 
-  return ( 
-  <button 
-  className={`${variants[variant]} ${disabled ? "opacity-50 cursor-not-allowed" : "hover:brightness-90"} ${className ?? ""}}`} 
-      onClick={onClick} 
-      disabled={disabled}>
+export default function Buttons({ text, onClick, variant, disabled = false, className }: ButtonsProps) {
+  return (
+    <button
+      className={`${variants[variant]} ${disabled ? "opacity-50 cursor-not-allowed" : "hover:brightness-90"} ${className ?? ""}}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {variantIcons[variant]}
       {text}
-  </button>
+    </button>
   );
 }
