f8ee1cc49446a1749e6456fabb8fd80de466d531
Amy <emysgalippa@gmail.com>
2025-09-24 17:15:55 +0200

    implemented clerk logic, fixed iu

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { navigation } from "./data/navigation";
+import { ClerkProvider } from "@clerk/clerk-react";
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        {navigation &&
-          navigation.length > 0 &&
-          navigation.map((route) => <Route key={route.path} path={route.path} element={route.element} />)}
-      </Routes>
-    </BrowserRouter>
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <BrowserRouter>
+        <Routes>
+          {navigation &&
+            navigation.length > 0 &&
+            navigation.map((route) => <Route key={route.path} path={route.path} element={route.element} />)}
+        </Routes>
+      </BrowserRouter>
+    </ClerkProvider>
   );
 }
--- src/pages/AuthContainer.tsx
+++ src/pages/AuthContainer.tsx
@@ -1,28 +1,30 @@
 import Form from "../components/form/Form";
 
+
 interface AuthContainerProps {
   isLogin: boolean;
 }
 
 export default function AuthContainer({ isLogin }: AuthContainerProps) {
-  {
-    /* non mi piace lo switch con il boolean, per il momento però lasciamolo */
-  }
+
 
   return (
-    <div className="flex min-h-screen flex-1   bg-dark text-white">
+    <div className="flex min-h-screen flex-1 bg-white text-gray-900">
       <div className="flex flex-1 flex-col justify-center px-4 py-12 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
         <div className="mx-auto w-full max-w-sm lg:w-96">
           <div>
-            <img className="h-10 w-auto" src="/logo.png" alt="Lost in AI" />
-            <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-white">
+            {/* <img className="h-10 w-auto" src="/logo.png" alt="Lost in AI" /> */}
+            <div className="bg-primary-500 p-4 rounded-md text-center">
+              <h1 className="text-3xl font-serif text-white ">Lost in AI</h1>
+            </div>
+            <h2 className="mt-8 text-2xl font-bold leading-9 tracking-tight text-gray-600">
               {isLogin ? "Accedi alla tua area riservata" : "Registrati"}
             </h2>
-            <p className="mt-2 text-sm leading-6 text-gray-400">
+            <p className="mt-2 text-sm leading-6 text-gray-600">
               {isLogin ? "Non hai un account ? " : "Hai già un account ?"}
               <a
                 href={isLogin ? "/register" : "/login"}
-                className="font-semibold text-indigo-500 hover:text-indigo-500"
+                className="font-semibold text-primary-500 hover:text-primary-500"
               >
                 {" "}
                 {isLogin ? "Registrati" : "Accedi"}
@@ -34,11 +36,11 @@ export default function AuthContainer({ isLogin }: AuthContainerProps) {
         </div>
       </div>
       <div className="relative hidden w-0 flex-1 lg:block">
-        {/*  TODO: Cambiamo foto, magari mettiamo qualcosa tema banca?  */}
+
         <img
-          className="absolute inset-0 h-full w-full object-cover"
-          src="https://images.unsplash.com/photo-1496917756835-20cb06e75b4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
-          alt=""
+          className="absolute inset-0 h-full w-full object-cover object-right "
+          src="https://images.unsplash.com/photo-1758518729711-1cbacd55efdb?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="sfondo dell'autenticazione"
         />
       </div>
     </div>
